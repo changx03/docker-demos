255ba145cf99f85d60ffcc53a0d94da62db9b0dc
Luke Chang <luke@boxfish.nz>
2025-03-21 11:55:26 +1300

    set username to the same as host

--- ros2/entrypoint.bash
+++ ros2/entrypoint.bash
@@ -3,11 +3,10 @@
 echo "UID: {$UID}, GUI: {$GID}"
 
 # use UID and GID as user and group
-groupadd -g $GID "group${GID}"
-useradd -u $UID -g $GID -M "user${UID}"
-chown -R "user${UID}":"group${GID}" /app
-export HOME=/app
+groupadd -g $GID anyone
+useradd -u $UID -g $GID -m anyone
+chown -R anyone:anyone /app
 
 # gosu allows user always has sudo permission in Docker container
-exec /usr/sbin/gosu "user${UID}" "$@"
+exec /usr/sbin/gosu anyone "$@"
 
