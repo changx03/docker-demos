242f2a19d7fc0d8308fb8182cb35584bcbfa30ca
Luke Chang <luke@boxfish.nz>
2025-03-21 16:02:03 +1300

    install iproute2 for ip

--- ros2/entrypoint.bash
+++ ros2/entrypoint.bash
@@ -3,10 +3,10 @@
 echo "UID: {$UID}, GUI: {$GID}"
 
 # use UID and GID as user and group
-groupadd -g $GID anyone
-useradd -u $UID -g $GID -m anyone
-chown -R anyone:anyone /app
+groupadd -g $GID user
+useradd -u $UID -g $GID -m anyogroupne
+chown -R $UID:$GID /app
 
 # gosu allows user always has sudo permission in Docker container
-exec /usr/sbin/gosu anyone "$@"
+exec /usr/sbin/gosu user "$@"
 
