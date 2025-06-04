15ab87c9af1cb88d910d7e3e69e41f398ae4ef99
Luke Chang <luke@boxfish.nz>
2025-06-04 14:32:41 +1200

    add ROS2 windows script

---
+++ ros2-windows/app/README.md
@@ -0,0 +1,9 @@
+# From the Docker container
+
+This file should be available in `/app` from the Docker container.
+
+Run the following to interact with the Docker container:
+
+```bash
+docker -it ros2-rviz2 bash
+```
---
+++ ros2-windows/run_docker_rviz2.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Running this script from WSL will launch RViz2 in Docker on Windows."
+
+docker build . -t ros2:humble_custom
+
+docker run -it --rm \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /mnt/wslg:/mnt/wslg \
+    -v /mnt/c/workspace/docker-demos/ros2-windows/app:/app \
+    -e DISPLAY \
+    -e WAYLAND_DISPLAY \
+    -e XDG_RUNTIME_DIR \
+    -e PULSE_SERVER \
+    -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+    --network host \
+    --name ros2-rviz2 \
+    ros2:humble_custom \
+    rviz2
